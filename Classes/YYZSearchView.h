af93cef1f6e669c9bed0df249d6b3c5bdaa596ac
yangyongzheng <youngyongzheng@qq.com>
2019-07-20 22:59:18 +0800

    自定义搜索

--- Classes/YYZSearchView.h
+++ Classes/YYZSearchView.h
@@ -3,7 +3,41 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+@class YYZSearchView;
+
+@protocol YYZSearchViewDelegate <NSObject>
+
+@optional
+- (BOOL)searchView:(YYZSearchView *)searchView textFieldShouldBeginEditing:(UITextField *)textField;
+- (void)searchView:(YYZSearchView *)searchView textFieldDidBeginEditing:(UITextField *)textField;
+- (BOOL)searchView:(YYZSearchView *)searchView textFieldShouldEndEditing:(UITextField *)textField;
+- (void)searchView:(YYZSearchView *)searchView textFieldDidEndEditing:(UITextField *)textField;
+
+- (BOOL)searchView:(YYZSearchView *)searchView textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string;
+- (void)searchView:(YYZSearchView *)searchView textFieldTextDidChange:(UITextField *)textField;
+
+- (BOOL)searchView:(YYZSearchView *)searchView textFieldShouldClear:(UITextField *)textField;
+- (BOOL)searchView:(YYZSearchView *)searchView textFieldShouldReturn:(UITextField *)textField;
+
+@end
+
 @interface YYZSearchView : UIView
+
+/** 请勿使用textField的代理，用YYZSearchView的代理代替 */
+@property (nonatomic, readonly, strong) UITextField *textField;
+@property (nonatomic, readonly, strong) UIButton *cancelButton;
+
+/**
+ 搜索icon颜色，默认值 #999999
+ */
+@property (nonatomic, strong) UIColor *searchIconColor;
+
+/**
+ 输入框背景色，默认值 #DDDDDD
+ */
+@property (nonatomic, strong) UIColor *inputBackgroundColor;
+
+@property (nonatomic, weak) id <YYZSearchViewDelegate> delegate;
 
 @end
 
--- YYZKit/ViewController.m
+++ YYZKit/ViewController.m
@@ -8,18 +8,35 @@
 
 #import "ViewController.h"
 #import "YYZKitHeader.h"
+#import "YYZSearchView.h"
 
-@interface ViewController ()
-
+@interface ViewController () <YYZSearchViewDelegate>
+{
+    YYZSearchView *searchView;
+}
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
     
+    UIView *titleView = [[UIView alloc] initWithFrame:self.navigationController.navigationBar.bounds];
+    searchView = [[YYZSearchView alloc] initWithFrame:CGRectMake(15, 0, titleView.frame.size.width-30, titleView.frame.size.height)];
+    searchView.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
+    [titleView addSubview:searchView];
+    [self.navigationController.navigationBar addSubview:titleView];
+    searchView.delegate = self;
 }
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    YYZSearchView *searchView = [[YYZSearchView alloc] initWithFrame:CGRectMake(15, 64, self.view.frame.size.width-30, 54)];
+    searchView.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleBottomMargin;
+    [self.view addSubview:searchView];
+}
+
+- (void)searchView:(YYZSearchView *)searchView textFieldTextDidChange:(UITextField *)textField {
+    NSLog(@"%@", textField.text);
+}
 
 @end
