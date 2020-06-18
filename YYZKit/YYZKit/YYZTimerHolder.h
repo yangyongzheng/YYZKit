1b35685ba547f9c3b09abb986c3768810e1f62fa
yangyongzheng <youngyongzheng@qq.com>
2020-06-18 15:00:12 +0800

    添加定时器

--- YYZKit/YYZKit/YYZTimerHolder.h
+++ YYZKit/YYZKit/YYZTimerHolder.h
@@ -12,6 +12,51 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface YYZTimerHolder : NSObject
 
+/// 常规定时器
+/// @param interval 间隔时间，单位秒，当 <= 0 时，使用默认值0.0001s
+/// @param repeats 是否重复触发定时器
+/// @param block 定时器触发回调
+- (void)scheduledTimerWithTimeInterval:(NSTimeInterval)interval
+                               repeats:(BOOL)repeats
+                                 block:(void (^)(YYZTimerHolder *timerHolder))block;
+
+/// 常规定时器
+/// @param interval 间隔时间，单位秒，当 <= 0 时，使用默认值0.0001s
+/// @param repeats 是否重复触发定时器
+/// @param mode 模式，默认 NSRunLoopCommonModes
+/// @param block 定时器触发回调
+- (void)scheduledTimerWithTimeInterval:(NSTimeInterval)interval
+                               repeats:(BOOL)repeats
+                                  mode:(NSRunLoopMode)mode
+                                 block:(void (^)(YYZTimerHolder *timerHolder))block;
+
+/// 倒计时定时器
+/// @param countdown 倒计时，单位秒
+/// @param interval 间隔时间，单位秒
+/// @param block 定时器触发回调
+- (void)scheduledTimerWithCountdown:(NSTimeInterval)countdown
+                           interval:(NSTimeInterval)interval
+                              block:(void (^)(YYZTimerHolder *timerHolder, NSTimeInterval timeRemaining))block;
+
+/// 倒计时定时器
+/// @param countdown 倒计时，单位秒
+/// @param interval 间隔时间，单位秒
+/// @param mode 模式，默认 NSRunLoopCommonModes
+/// @param block 定时器触发回调
+- (void)scheduledTimerWithCountdown:(NSTimeInterval)countdown
+                           interval:(NSTimeInterval)interval
+                               mode:(NSRunLoopMode)mode
+                              block:(void (^)(YYZTimerHolder *timerHolder, NSTimeInterval timeRemaining))block;
+
+/// 立即触发一次定时器
+- (void)fireTimer;
+
+/// 停止定时器
+- (void)invalidateTimer;
+
+/// 定时器是否有效
+@property (readonly) BOOL isValid;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- YYZKit/YYZKit/YYZTimerHolder.m
+++ YYZKit/YYZKit/YYZTimerHolder.m
@@ -8,6 +8,134 @@
 
 #import "YYZTimerHolder.h"
 
+@interface YYZKitPrivateTimerTarget : NSObject
+
+@end
+
+@implementation YYZKitPrivateTimerTarget
+
++ (NSTimer *)scheduledTimerWithTimeInterval:(NSTimeInterval)interval
+                                    repeats:(BOOL)repeats
+                                       mode:(NSRunLoopMode)mode
+                                      block:(void (^)(NSTimer *timer))block {
+    if (interval <= 0) {
+        interval = 0.0001;
+    }
+    NSTimer *timer = [NSTimer timerWithTimeInterval:interval
+                                             target:self
+                                           selector:@selector(timerDidFireCallback:)
+                                           userInfo:block ? [block copy] : nil
+                                            repeats:repeats];
+    [NSRunLoop.currentRunLoop addTimer:timer forMode:mode?:NSRunLoopCommonModes];
+    return timer;
+}
+
++ (void)timerDidFireCallback:(NSTimer *)timer {
+    void (^ block)(NSTimer *) = timer.userInfo;
+    if (block) {
+        block(timer);
+    } else {
+        [timer invalidate];
+    }
+}
+
+@end
+
+
+
+@interface YYZTimerHolder ()
+@property (nonatomic, strong) NSTimer *currentTimer;
+@property (nonatomic) NSTimeInterval timeRemaining;
+@end
+
 @implementation YYZTimerHolder
+
+- (void)scheduledTimerWithTimeInterval:(NSTimeInterval)interval
+                               repeats:(BOOL)repeats
+                                 block:(void (^)(YYZTimerHolder * _Nonnull))block {
+    [self scheduledTimerWithTimeInterval:interval
+                                 repeats:repeats
+                                    mode:NSRunLoopCommonModes
+                                   block:block];
+}
+
+- (void)scheduledTimerWithTimeInterval:(NSTimeInterval)interval
+                               repeats:(BOOL)repeats
+                                  mode:(NSRunLoopMode)mode
+                                 block:(void (^)(YYZTimerHolder * _Nonnull))block {
+    [self invalidateTimer];
+    if (interval <= 0) {interval = 0.0001;}
+    if (block) {
+        __weak typeof(self) weakSelf = self;
+        self.currentTimer = [YYZKitPrivateTimerTarget scheduledTimerWithTimeInterval:interval
+                                                                             repeats:repeats
+                                                                                mode:mode
+                                                                               block:^(NSTimer *timer) {
+            __strong typeof(weakSelf) strongSelf = weakSelf;
+            if (strongSelf) {
+                block(strongSelf);
+                if (!repeats) {
+                    [strongSelf invalidateTimer];
+                }
+            }
+        }];
+    }
+}
+
+- (void)scheduledTimerWithCountdown:(NSTimeInterval)countdown
+                           interval:(NSTimeInterval)interval
+                              block:(void (^)(YYZTimerHolder * _Nonnull, NSTimeInterval))block {
+    [self scheduledTimerWithCountdown:countdown
+                             interval:interval
+                                 mode:NSRunLoopCommonModes
+                                block:block];
+}
+
+- (void)scheduledTimerWithCountdown:(NSTimeInterval)countdown
+                           interval:(NSTimeInterval)interval
+                               mode:(NSRunLoopMode)mode
+                              block:(void (^)(YYZTimerHolder * _Nonnull, NSTimeInterval))block {
+    [self invalidateTimer];
+    self.timeRemaining = countdown;
+    if (interval <= 0) {interval = 0.0001;}
+    if (block && interval <= countdown) {
+        __weak typeof(self) weakSelf = self;
+        self.currentTimer = [YYZKitPrivateTimerTarget scheduledTimerWithTimeInterval:interval
+                                                                             repeats:YES
+                                                                                mode:mode
+                                                                               block:^(NSTimer *timer) {
+            __strong typeof(weakSelf) strongSelf = weakSelf;
+            if (strongSelf) {
+                strongSelf.timeRemaining -= interval;
+                if (strongSelf.timeRemaining <= 0) {
+                    strongSelf.timeRemaining = 0;
+                    [strongSelf invalidateTimer];
+                }
+                block(strongSelf, strongSelf.timeRemaining);
+            }
+        }];
+    }
+}
+
+- (void)fireTimer {
+    if (self.currentTimer) {
+        [self.currentTimer fire];
+    }
+}
+
+- (void)invalidateTimer {
+    if (self.currentTimer) {
+        [self.currentTimer invalidate];
+        self.currentTimer = nil;
+    }
+}
+
+- (BOOL)isValid {
+    return self.currentTimer ? self.currentTimer.isValid : NO;
+}
+
+- (void)dealloc {
+    [self invalidateTimer];
+}
 
 @end
